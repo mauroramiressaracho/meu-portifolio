1b79bf75a5198b94def3fb91840063970e3ef4c1
Mauro Ramires <mauroramirescg@gmail.com>
2026-03-04 12:40:38 -0400

    inclusao de empresas que desenvolvi

---
+++ data/empresas.js
@@ -0,0 +1,26 @@
+window.portfolioCompanies = [
+    {
+        name: 'Sertão',
+        logo: 'images/empresas/sertao.png'
+    },
+    {
+        name: 'Governo MS',
+        logo: 'images/empresas/governoms.jpg'
+    },
+    {
+        name: 'El Shaddai',
+        logo: 'images/empresas/elshaddai.jpg'
+    },
+    {
+        name: 'Inovvati',
+        logo: 'images/empresas/inovvati.jpg'
+    },
+    {
+        name: 'Estacio',
+        logo: 'images/empresas/estacio.png'
+    },
+    {
+        name: 'Ernesto Borges',
+        logo: 'images/empresas/ernestoborges.png'
+    },
+];
---
+++ images/empresas/README.txt
@@ -0,0 +1,7 @@
+Coloque aqui os arquivos de logo das empresas.
+
+Depois ajuste a lista em:
+data/empresas.js
+
+Exemplo:
+{ name: 'Minha Empresa', logo: 'images/empresas/minha-empresa.png' }
